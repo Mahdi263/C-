ec2f4f5e0023ecca10ef27d01a4eb746b74f096e
Mahdi263 <g80128010@gmail.com>
2025-04-29 09:35:28 -0700

    Second commit for C#

---
+++ Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            int c = a + b;
+            c = a - b;
+            c = a * b;
+            Console.WriteLine(c);
+            string name = "Mahdi";
+            string name2 = "Moahmmad";
+            Console.WriteLine(name);
+            Console.WriteLine(name2);
+            Console.ReadKey();
+        }
+    }
+}
