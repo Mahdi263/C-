e8c103ed0d8e27f68c109fc636c8e0b22ba0fa4a
Mahdi263 <g80128010@gmail.com>
2025-04-29 09:10:01 -0700

    First commit for C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Wellcome to the c#");
+            Console.Write("Mohammad Mahdi");
+            Console.ReadKey();
+        }
+    }
+}
