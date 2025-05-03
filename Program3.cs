57583c09a76a7ed2d6a3b3a13ab6acac318bc8f7
Mahdi263 <g80128010@gmail.com>
2025-05-03 04:07:12 -0700

    Add program3.cs by Mohammad Mahdi

---
+++ Program3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int a = 50;
+            //int b = i + a;
+            //int z = a / i;
+            string s = "Mohammad";
+            string v = "Mahdi";
+            string f = s + v;
+            Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine(z);
+            //Console.WriteLine(b);
+            Console.WriteLine(f);
+            Console.ReadKey();
+        }
+    }
+}
